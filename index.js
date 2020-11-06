044a82f0c6e679968a8112b0fa8fd83df5c9232a
hamed6 <h.ghodsinezhad@gmail.com>
2020-11-06 22:32:10 +0200

    Object literals

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// object literals
+const drawing={
+    length:5,
+    width:5,
+    location:{
+        x:1,
+        y:1
+    },
+    calulate:function(){
+        console.log(this.length*this.width);
+    }
+}
 
-
-console.log('test');
+drawing.calulate();
