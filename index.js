f7a521a294658e05f56fcc5e1f3867792b1c4a47
hamed6 <h.ghodsinezhad@gmail.com>
2020-11-07 21:31:58 +0200

    functionsObject

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ const drawing={
 }
 
 drawing.calulate();
+// 2 ways to create an object 1st factories 2nd constructors
+
+// factory function
+ function createCir(radius){
+    return{
+        radius, // it is ES6 feature, it means radius = radius IF key and value are same.
+        draw: function(){
+            console.log(`factories ${radius}`);
+        }
+    }
+}
+
+createCir(10).draw();
+
+// constructor function
+// constructor names start with Capital + no need to write return as NEW will create an object then return 
+function Create(val){
+    this.val=val;
+    this.width=function(){
+        console.log('val',this.val);
+    }
+}
+
+const newCreate = new Create(20);
+newCreate.width();
